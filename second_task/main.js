494571bcf263519d9da9efe1a4ba4fd2c2878282
AvokinLokos <lerasokolnikova2@gmail.com>
2022-04-20 21:32:14 +0300

    Finished task 6

--- second_task/main.js
+++ second_task/main.js
@@ -35,8 +35,25 @@ function calculate(first, second, sign){
 
 let MathFrameWork = {
     result: "",
+    first:0,
+    second:0,
     calc: (a,b,sign) => MathFrameWork.result = calculate(a,b,sign)
 }
 
-MathFrameWork.calc({a: 7}, {b: 2}, "-")
+// MathFrameWork.calc({a: 7}, {b: 2}, "-")
+// console.log(MathFrameWork.result)
+
+MathFrameWork.first = [2,4]
+MathFrameWork.second = [5,7]
+MathFrameWork.calc(MathFrameWork.first, MathFrameWork.second, "+")
+console.log(MathFrameWork.result)
+
+MathFrameWork.first = 2
+MathFrameWork.second = 5
+MathFrameWork.calc(MathFrameWork.first, MathFrameWork.second, "*")
+console.log(MathFrameWork.result)
+
+MathFrameWork.first = {a: 25}
+MathFrameWork.second = {b: 3}
+MathFrameWork.calc(MathFrameWork.first, MathFrameWork.second, "*")
 console.log(MathFrameWork.result)
