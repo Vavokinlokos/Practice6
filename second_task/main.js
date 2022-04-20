0628ba067bbdbbdbf500830ba869a9200783031e
AvokinLokos <lerasokolnikova2@gmail.com>
2022-04-20 20:59:48 +0300

    Finished second task

--- second_task/main.js
+++ second_task/main.js
@@ -21,5 +21,16 @@ function calculate(first, second, sign){
             return first / second
         }
     }
+    if (Array.isArray(first)){
+        let result = []
+        first.forEach((num1, index) => {
+            const num2 = second[index]
+            result.push(signs[sign](num1, num2))
+        })
+        return result
+    }
     return signs[sign](first, second)
 }
+
+console.log(calculate([1,2,3], [1,5,3], "+"))
+console.log(calculate([1,2,3], [1,5,3], "*"))
